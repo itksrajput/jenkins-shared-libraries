0efce3bd06f0d60058754de96711c3f143883412
kalusingh rajput <87700418+itksrajput@users.noreply.github.com>
2025-12-14 11:31:30 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,10 @@
-def call(String Project, String ImageTag){
-  sh "docker build -t ${Project}:${ImageTag} ."
-}
+docker_build(
+  imageName: 'laxg66/easyshop-app',
+  imageTag: '10',
+  dockerfile: 'Dockerfile'
+)
+
+
+#def call(String Project, String ImageTag){
+ # sh "docker build -t ${Project}:${ImageTag} ."
+#}
