45972abe23fe50c1af98e7c7fa775396bc7cf009
kalusingh rajput <87700418+itksrajput@users.noreply.github.com>
2025-12-14 11:33:45 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,8 +1,28 @@
-docker_build(
-  imageName: 'laxg66/easyshop-app',
-  imageTag: '10',
-  dockerfile: 'Dockerfile'
-)
+def call(Map args) {
+    echo "Docker build args: ${args}"
+
+    sh '''
+      set -x
+      whoami
+      pwd
+      ls -la
+      docker --version
+      docker info
+    '''
+
+    def imageName  = args.imageName
+    def imageTag   = args.imageTag
+    def dockerfile = args.dockerfile ?: 'Dockerfile'
+    def context    = args.context ?: '.'
+
+    sh """
+      set -x
+      docker build \
+        -t ${imageName}:${imageTag} \
+        -f ${dockerfile} \
+        ${context}
+    """
+}
 
 
 #def call(String Project, String ImageTag){
